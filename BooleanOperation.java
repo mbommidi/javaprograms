6b10167b25786be5de1818e66a3999d9355a2967
mbommidi <107984464+mbommidi@users.noreply.github.com>
2022-07-14 13:34:52 +0530

    Add files via upload

---
+++ BooleanOperation.java
@@ -0,0 +1,21 @@
+//JFM1T3_Assignment5:
+/*
+1.Write a program to initialize a boolean variable and print it's opposite value on console.
+
+  Sample Input:true
+
+  Expected Output:false
+
+*/
+public class BooleanOperation {
+  public static void main(String args[]){
+ boolean Status = true;
+    System.out.println("Status is "+(!Status));
+   
+//Define main method
+
+//Declare a variable and initialize it as true or false 
+
+//Print the Result using not operator 
+  }
+}
---
+++ Circle.java
@@ -0,0 +1,42 @@
+//JFM1T3_Assignment1: 
+/* Write a program to calculate the circumference of a circle and print the result in whole number. 
+  Allow user to input the cirlce radius from terminal. 
+
+  Sample Input:
+    radius of the circle=6
+
+  Expected Output:
+   Circumference of the circle=38
+   
+
+*/ 
+
+//import statement for java program to read inputs using Scanner class 
+import java.util.Scanner;
+
+public class Circle { 
+public static void main(String args[])
+  {
+//Define main method
+
+//Declare the variables
+int radius;
+
+
+//Use the scanner class to provide radius at execution time
+
+
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter radius as a positive integer");
+radius= scanner.nextInt();
+    double circumference = 2*3.14*radius;
+System.out.println("circumference of circle is"+ circumference);
+}
+
+//Caluculate the circumference of the circle
+
+//Casting the floating-point value to int  
+
+//Print the Result
+
+} 
