95ac4b3343ed224db02cccd47c257f5aeed0b7b9
mbommidi <107984464+mbommidi@users.noreply.github.com>
2022-07-14 13:30:01 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,10 @@
+// Write a program to print your Name, Father's Name and Mailing Address in different lines
+
+class Exercise1 {
+public static void main (String org[])
+  {
+System.out.println ("Manohar");
+System.out.println ("Srinivasa rao");
+System.out.println ("manoharbommodi1998@gmail.com");
+  }
+}
---
+++ Sample1.java
@@ -0,0 +1,10 @@
+// Write a program to print your name
+
+public class Sample1 {
+
+	public static void main(String args[]) {
+
+	//Print your name in Console
+	System.out.print ("Manohar");
+	}
+}
